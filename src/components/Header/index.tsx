4d47b25523017e7c642980ffc382d7e861a527ac
Joao Gilberto <joaogjoao11@hotmail.com>
2021-10-11 11:37:58 -0300

    Receive data from Prismic CMS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
+
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 
 import styles from './styles.module.scss';
 
@@ -10,8 +12,12 @@ export function Header() {
         <Image src="/images/logo.svg" alt="ig.news" width={110} height={31} />
 
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
